95e35ec919c1720c0087718c10d0d41f46908698
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-09-01 21:25:55 +0300

    Added classification code copied from iris-ml-logistic-regression-solver

---
+++ main.py
@@ -0,0 +1,32 @@
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+import matplotlib.pyplot as plt
+
+# Load dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Scale data
+X = StandardScaler().fit(X).transform(X)
+
+# Split data into train and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+# Train model
+# clf = LogisticRegression(random_state=0)
+clf.fit(X_train, y_train)
+
+# Classify test data using model
+y_pred = clf.predict(X_test)
+
+# Compute the confusion matrix
+disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, y_pred), display_labels=iris.target_names)
+disp.plot()
+plt.show()
+
+# Get the model classification metrics (will only show after the confusion matrix display window is closed)
+print(classification_report(y_test, y_pred, target_names=iris.target_names))
